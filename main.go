a1a3e8fad28e38258b197edd224517bd49bcc86e
damirkap89 <damirkap89@gmail.com>
2021-11-08 21:36:39 +0600

    added func server

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	// "fmt"
 	grtrack "grtrack/struct"
 	"net/http"
+	"time"
 )
 
 //include templates
@@ -11,4 +12,16 @@ func main() {
 	http.HandleFunc("/main", grtrack.HundlerFirst)
 	http.HandleFunc("/bands/", grtrack.HundlerSecond)
 	http.ListenAndServe(":8080", nil)
+}
+
+func (s *Server) Run() error {
+	handler := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:         s.Address,
+		Handler:      handler,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 10,
+	}
+	return srv.ListenAndServe()
 }
